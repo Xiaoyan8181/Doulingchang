7c7992ec04dfedbaf167709e3c41d4c00334b41a
Xiaoyan8181 <xiaoyan8181@gmail.com>
2025-04-07 01:49:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,146 +1,105 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
-const fs = require('fs');
-const path = require('path');
+const socketIo = require('socket.io');
+const bcrypt = require('bcrypt');
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: "https://xiaoyan8181.github.io",
-        methods: ["GET", "POST"]
-    }
-});
+const io = socketIo(server);
 
-const DATA_FILE = path.join('/tmp', 'users.json');
-let users = {};
-let rooms = {};
-let playerCoins = {};
+app.use(express.static('public'));
 
-function loadUsers(callback) {
-    fs.readFile(DATA_FILE, 'utf8', (err, data) => {
-        if (err) {
-            users = {};
-            playerCoins = {};
-            saveUsers(() => callback());
+let users = [];
+let rooms = [];
+
+io.on('connection', (socket) => {
+    console.log('新用戶已連線');
+
+    socket.on('register', async (data) => {
+        const { username, password } = data;
+        const existingUser = users.find(user => user.username === username);
+        if (existingUser) {
+            socket.emit('registerFailure');
         } else {
-            const parsed = JSON.parse(data);
-            users = parsed.users || {};
-            playerCoins = parsed.playerCoins || {};
-            callback();
+            const hashedPassword = await bcrypt.hash(password, 10);
+            users.push({ username, password: hashedPassword });
+            socket.emit('registerSuccess');
         }
     });
-}
 
-function saveUsers(callback) {
-    fs.writeFile(DATA_FILE, JSON.stringify({ users, playerCoins }, null, 2), callback);
-}
+    socket.on('login', async (data) => {
+        const { username, password } = data;
+        const user = users.find(user => user.username === username);
+        if (user && await bcrypt.compare(password, user.password)) {
+            socket.emit('loginSuccess', { username });
+        } else {
+            socket.emit('loginFailure');
+        }
+    });
 
-loadUsers(() => {
-    io.on('connection', (socket) => {
-        socket.on('register', ({ username, password }, callback) => {
-            if (users[username]) {
-                callback({ success: false, message: '帳號名已註冊過' });
-            } else {
-                users[username] = password;
-                playerCoins[username] = 10000000; // 修改：初始鬥靈幣為 10000000
-                saveUsers(() => {
-                    callback({ success: true, message: '已成功註冊' });
-                });
-            }
-        });
+    socket.on('createRoom', (room) => {
+        rooms.push(room);
+        socket.join(room.id);
+        io.emit('updateRooms', rooms);
+    });
 
-        socket.on('login', ({ username, password }, callback) => {
-            if (users[username] && users[username] === password) {
-                socket.username = username;
-                callback({ success: true });
-            } else {
-                callback({ success: false, message: '帳號或密碼錯誤' });
-            }
-        });
+    socket.on('getRooms', () => {
+        socket.emit('updateRooms', rooms);
+    });
 
-        // 新增：獲取鬥靈幣數
-        socket.on('getCoins', ({ username }, callback) => {
-            callback(playerCoins[username] || 0);
-        });
-
-        // 新增：每日簽到
-        socket.on('checkIn', ({ username }, callback) => {
-            playerCoins[username] = (playerCoins[username] || 0) + 1000000; // 增加 1000000 鬥靈幣
-            saveUsers(() => {
-                callback({ success: true, coins: playerCoins[username] });
-            });
-        });
-
-        socket.on('getRooms', (callback) => {
-            callback(rooms);
-        });
-
-        socket.on('createRoom', (room, callback) => {
-            const roomId = Math.random().toString(36).substring(7);
-            rooms[roomId] = {
-                ...room,
-                players: [socket.username],
-                status: 'open',
-                spirits: [],
-                props: {},
-                bets: {}
-            };
+    socket.on('joinRoom', ({ roomId, username }) => {
+        const room = rooms.find(r => r.id === roomId);
+        if (room) {
             socket.join(roomId);
-            io.emit('roomUpdate', rooms);
-            callback({ success: true, roomId });
-        });
+            // 確保玩家不重複
+            if (!room.players.some(p => p.username === username)) {
+                room.players.push({ username });
+            }
+            io.to(roomId).emit('playerJoined', { roomId, username });
+            io.emit('updateRooms', rooms);
+        }
+    });
 
-        socket.on('joinRoom', ({ roomId, username }, callback) => {
-            const room = rooms[roomId];
-            if (!room) {
-                callback({ success: false, message: '房間不存在' });
-            } else if (room.isPublic || room.password === '' || prompt('輸入房間密碼') === room.password) {
-                if (room.limit && room.players.length >= room.limit) {
-                    callback({ success: false, message: '房間已滿' });
+    socket.on('leaveRoom', ({ roomId, username }) => {
+        const room = rooms.find(r => r.id === roomId);
+        if (room) {
+            room.players = room.players.filter(p => p.username !== username);
+            io.to(roomId).emit('playerLeft', { roomId, username });
+            io.emit('updateRooms', rooms);
+        }
+    });
+
+    socket.on('closeRoom', (roomId) => {
+        rooms = rooms.filter(r => r.id !== roomId);
+        io.to(roomId).emit('roomClosed', roomId);
+        io.emit('updateRooms', rooms);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('用戶已斷線');
+        // 檢查是否有房間需要關閉
+        rooms.forEach(room => {
+            const player = room.players.find(p => p.username === socket.username);
+            if (player) {
+                if (room.owner === socket.username) {
+                    // 房主斷線，關閉房間
+                    rooms = rooms.filter(r => r.id !== room.id);
+                    io.to(room.id).emit('roomClosed', room.id);
                 } else {
-                    room.players.push(username);
-                    socket.join(roomId);
-                    io.emit('roomUpdate', rooms);
-                    callback({ success: true });
+                    // 非房主斷線，移除玩家
+                    room.players = room.players.filter(p => p.username !== socket.username);
+                    io.to(room.id).emit('playerLeft', { roomId: room.id, username: socket.username });
                 }
-            } else {
-                callback({ success: false, message: '密碼錯誤' });
             }
         });
-
-        socket.on('startGame', (roomId) => {
-            const room = rooms[roomId];
-            if (room.owner === socket.username) {
-                room.status = 'playing';
-                room.spirits = Array(4).fill().map(() => Math.floor(Math.random() * 33));
-                io.to(roomId).emit('gameStarted', { spirits: room.spirits, feedTime: room.feedTime });
-            }
-        });
-
-        socket.on('buyProp', ({ roomId, prop, target }, callback) => {
-            const room = rooms[roomId];
-            const cost = { '+0~2': 200, '-0~2': 200, '+2~4': 400, '-2~4': 400, '骰子+1': 600, '點數=1': 600 }[prop];
-            if (playerCoins[socket.username] >= cost) {
-                playerCoins[socket.username] -= cost;
-                room.props[target] = room.props[target] || [];
-                room.props[target].push(prop);
-                io.to(roomId).emit('updateProps', room.props);
-                io.to(socket.id).emit('updateCoins', playerCoins[socket.username]);
-                saveUsers(() => {});
-            }
-        });
-
-        socket.on('refreshProps', (roomId) => {
-            if (playerCoins[socket.username] >= 50) {
-                playerCoins[socket.username] -= 50;
-                io.to(socket.id).emit('updateCoins', playerCoins[socket.username]);
-                saveUsers(() => {});
-            }
-        });
+        io.emit('updateRooms', rooms);
     });
 
-    server.listen(process.env.PORT || 3000, () => {
-        console.log('listening on port 3000');
+    // 儲存用戶名以便在斷線時使用
+    socket.on('loginSuccess', (user) => {
+        socket.username = user.username;
     });
+});
+
+server.listen(3000, () => {
+    console.log('伺服器運行於 http://localhost:3000');
 });
